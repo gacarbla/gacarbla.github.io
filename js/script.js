e631f9220d0a9901f49de1cb452bc45d65960db4
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-06-06 22:06:08 +0200

    Actualización

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+var carga = true
+var warnedConsole = false
+var oldWidth = 0
+var oldHeigh = 0
+
+
+var error_working = "Lo sentimos, estamos trabajando en esto y por el momento no está dispoible esta función."
+
+
+
 function light() {
     window.alert("Esta función es experimental y podría causar errores en la página")
     document.getElementById('body').classList.remove('dark')
     document.getElementById('body').classList.add('light')
     document.getElementById('id-moon').classList.remove('active')
     document.getElementById('id-sun').classList.add('active')
+    return "Se ha activado el modo claro en la página"
 }
 
 function dark() {
@@ -11,6 +22,7 @@ function dark() {
     document.getElementById('body').classList.remove('light')
     document.getElementById('id-sun').classList.remove('active')
     document.getElementById('id-moon').classList.add('active')
+    return "Se ha activado el modo oscuro en la página"
 }
 
 function goPage(page) {
@@ -37,21 +49,63 @@ function copyCode(id) {
         var text = document.getElementById(`${id}`)
         text.select()
         document.execCommand("copy")
+        return "Se ha copiado el texto solicitado y se encuentra ahora en el portapapeles"
     }
 }
 
 function load(){
-    start();
-    window.addEventListener('resize', start);
+    carga = true
+    windowSize();
+    window.addEventListener('resize', windowSize);
+    return "Recargando la página"
 }
 
-function start(){
+function windowSize(){
+    var variable = false
     var width = document.documentElement.clientWidth
     var height = document.documentElement.clientHeight
-    console.log(width, height)
-    if(height>width){
-        document.getElementById("page").className = "pagem"
-    } else {
-        document.getElementById("page").className = "page"
+    if (oldHeigh==0 || oldWidth==0 || oldHeigh==width || oldWidth==height ) {
+        oldHeigh=height
+        oldWidth=width
+        variable = true
     }
+    if(carga || warnedConsole || variable){
+        if(height>width){
+            document.getElementById("page").className = "pagem"
+        } else {
+            document.getElementById("page").className = "page"
+        }
+        setTimeout(function(){
+            carga = false
+        }, 1000)
+    } else {
+        if(!warnedConsole) {
+            warnedConsole = true
+            window.alert("Se ha detectado el uso de la consola para la manipulación de la página. Se ruega a los usuarios que utilicen este medio, lean la documentación de uso de la consola.\nSi deseas saber cómo acceder a esta, use en la consola el comando cmdDocs()")
+        }
+    }
+    return "Iniciado sistema automático de detección de cambios de pantalla"
+}
+
+function cmdDosc(page) {
+    pages = {
+        1: {
+            name: "",
+            url: ""
+        },
+    }
+
+    if(!page){
+        
+    } else {
+        return error_working
+    }
+}
+
+function helpCmd(command) {
+
+}
+
+function help() {
+
 }
