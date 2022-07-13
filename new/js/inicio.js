551286ed96f47262a1da0eda59a19a9fc7e223be
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-07-13 15:47:40 +0200

    Actualización

--- new/js/inicio.js
+++ new/js/inicio.js
@@ -11,3 +11,17 @@ function campoBlur(campo){
 function click(object) {
     console.log("1")
 }
+
+function logIn() {
+    document.getElementById("SingUp").hidden = true
+    document.getElementById("LogIn").hidden = false
+    document.getElementById("dereita").className = "activo"
+    document.getElementById("izquierda").className = "inactivo"
+}
+
+function singUp() {
+    document.getElementById("SingUp").hidden = false
+    document.getElementById("LogIn").hidden = true
+    document.getElementById("dereita").className = "inactivo"
+    document.getElementById("izquierda").className = "activo"
+}
