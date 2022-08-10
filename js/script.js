15c5a8652b9ff5c435a6b5d7e7920e480692c4e6
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-08-10 17:39:25 +0200

    Actualización

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const navigation = {
         name: "Perfil",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 8V6a2 2 0 0 1 2-2h2"></path><path d="M4 16v2a2 2 0 0 0 2 2h2"></path><path d="M16 4h2a2 2 0 0 1 2 2v2"></path><path d="M16 20h2a2 2 0 0 0 2-2v-2"></path><path d="M9 10h.01"></path><path d="M15 10h.01"></path><path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path></svg>',
         class: "pagina",
-        onclick: "go('https://github.com/gacarbla', false)",
+        onclick: "go('https://github.com/gacarbla', true)",
     },
     "6": {
         name: "gacarbla.github.io",
@@ -170,13 +170,11 @@ function load() {
 /* ELEMENTAL FUNCTIONS */
 
 function go(page, newTabBoolean) {
-    
-    page.replace(":", "https://gacarbla.github.io/")
-
+    if (page.startsWith(":")) page = page.replace(":", "https://gacarbla.github.io/")
     if (newTabBoolean) {
-        window.open(`${pagina}`, "GΛCΛRBLΛ", "width=1080, height=440")
+        window.open(`${page}`, "GΛCΛRBLΛ", "width=1080, height=440")
     } else {
-        window.location = `${pagina}`
+        window.location = `${page}`
     }
 }
 
