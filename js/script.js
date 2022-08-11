62f5ba132f22589d1f10c769a8432d7efa54eec6
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-08-11 12:22:06 +0200

    Actualización

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
+var warned = false;
+
 const navigation = {
     "0": {
         name: "Inicio",
         vector: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5 12H3l9-9 9 9h-2"></path><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-7"></path><path d="M9 21v-6a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v6"></path></svg>`,
         class: "pagina",
-        onclick: "go(':', false)"
+        onclick: "go(':', false)",
+        title: "Vuelve a la página de inicio",
+        disabled: false
     },
     "1": {
         name: "Herramientas",
         vector: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 21h4L20 8a2.829 2.829 0 0 0-4-4L3 17v4Z"></path><path d="m14.5 5.5 4 4"></path><path d="M12 8 7 3 3 7l5 5"></path><path d="M7 8 5.5 9.5"></path><path d="m16 12 5 5-4 4-5-5"></path><path d="m16 17-1.5 1.5"></path></svg>`,
-        class: "separador",
+        class: "separador"
     },
     "2": {
         name: "Timestamp",
         vector: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Z"></path><path d="M12 7v5l3 3"></path></svg>`,
         class: "pagina",
         onclick: "go(':herramientas/timestamp', false)",
+        title: "Discord Timestamp Generator",
+        disabled: true
     },
     "3": {
         name: "Codificador",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17 11H7a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2Z"></path><path d="M12 17a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path><path d="M8 11V7a4 4 0 0 1 8 0v4"></path></svg>',
         class: "pagina",
         onclick: "go(':herramientas/codificador', false)",
+        title: "Codificador simple de textos",
+        disabled: true
     },
     "4": {
         name: "GitHub",
@@ -32,36 +40,48 @@ const navigation = {
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 8V6a2 2 0 0 1 2-2h2"></path><path d="M4 16v2a2 2 0 0 0 2 2h2"></path><path d="M16 4h2a2 2 0 0 1 2 2v2"></path><path d="M16 20h2a2 2 0 0 0 2-2v-2"></path><path d="M9 10h.01"></path><path d="M15 10h.01"></path><path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path></svg>',
         class: "pagina",
         onclick: "go('https://github.com/gacarbla', true)",
+        title: "Accede al perfil de GitHub de gacarbla",
+        disabled: false
     },
     "6": {
         name: "gacarbla.github.io",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20 4-2 14.5-6 2-6-2L4 4h16Z"></path><path d="M15.5 8h-7l.5 4h6l-.5 3.5-2.5.75-2.5-.75-.1-.5"></path></svg>',
         class: "pagina",
-        onclick: "",
+        onclick: "go('https://github.com/gacarbla/gacarbla.github.io', true)",
+        title: "Accede al repositorio",
+        disabled: false
     },
     "7": {
         name: "Dori",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20 4-2 14.5-6 2-6-2L4 4h16Z"></path><path d="M7.5 8h3v8l-2-1"></path><path d="M16.5 8H14a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1.423a.5.5 0 0 1 .495.57L15.5 15.5l-2 .5"></path></svg>',
         class: "pagina",
-        onclick: "",
+        onclick: "go('https://github.com/gacarbla/Dori', true)",
+        title: "Accede al repositorio",
+        disabled: false
     },
     "8": {
         name: "NewsBot",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20 4-2 14.5-6 2-6-2L4 4h16Z"></path><path d="M7.5 8h3v8l-2-1"></path><path d="M16.5 8H14a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1.423a.5.5 0 0 1 .495.57L15.5 15.5l-2 .5"></path></svg>',
         class: "pagina",
-        onclick: "",
+        onclick: "go('https://github.com/gacarbla/NewsBot', true)",
+        title: "Accede al repositorio",
+        disabled: false
     },
     "9": {
         name: "discord.js v13 guide",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20 4-2 14.5-6 2-6-2L4 4h16Z"></path><path d="M7.5 8h3v8l-2-1"></path><path d="M16.5 8H14a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1.423a.5.5 0 0 1 .495.57L15.5 15.5l-2 .5"></path></svg>',
         class: "pagina",
-        onclick: "",
+        onclick: "go('https://github.com/gacarbla/discordjsv13guide', true)",
+        title: "Accede al repositorio",
+        disabled: false
     },
     "10": {
         name: "Computer analyzer",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 9H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h3"></path><path d="M12 15h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3"></path><path d="M8 9V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-4"></path><path d="M11 6v.01"></path><path d="M13 18v.01"></path></svg>',
         class: "pagina",
-        onclick: "",
+        onclick: "go('https://github.com/gacarbla/computerAnalyzer', true)",
+        title: "Accede al repositorio",
+        disabled: false
     },
     "11": {
         name: "Página",
@@ -74,18 +94,24 @@ const navigation = {
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M14 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"></path><path d="M4 6h8"></path><path d="M16 6h4"></path><path d="M8 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"></path><path d="M4 12h2"></path><path d="M10 12h10"></path><path d="M17 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"></path><path d="M4 18h11"></path><path d="M19 18h1"></path></svg>',
         class: "pagina",
         onclick: "newWindow('settings')",
+        title: "Modifica el aspecto de la página a tu gusto",
+        disabled: false
     },
     "13": {
         name: "Cookies",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8 13v.01"></path><path d="M12 17v.01"></path><path d="M12 12v.01"></path><path d="M16 14v.01"></path><path d="M11 8v.01"></path><path d="m13.148 3.478 2.667 1.104a4 4 0 0 0 4.656 6.14l.053.132a3 3 0 0 1 0 2.296c-.497.786-.838 1.404-1.024 1.852-.189.456-.409 1.194-.66 2.216a3 3 0 0 1-1.624 1.623c-1.048.263-1.787.483-2.216.661-.475.197-1.092.538-1.852 1.024a3 3 0 0 1-2.296 0c-.802-.503-1.419-.844-1.852-1.024-.47-.195-1.21-.415-2.216-.66a3 3 0 0 1-1.623-1.624c-.265-1.052-.485-1.79-.66-2.216-.199-.479-.54-1.096-1.025-1.852a3 3 0 0 1 0-2.296c.48-.744.82-1.36 1.024-1.852.171-.413.391-1.152.66-2.216a3 3 0 0 1 1.624-1.623c1.032-.256 1.77-.476 2.216-.66.458-.19 1.075-.532 1.852-1.025a3 3 0 0 1 2.296 0v0Z"></path></svg>',
         class: "pagina",
         onclick: "newWindow('cookies')",
+        title: "Modifica el uso de Cookies a tu gusto",
+        disabled: false
     },
     "14": {
         name: "Ajustes dev",
         vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M9 12h6"></path><path d="M12 9v6"></path><path d="M6 19a2 2 0 0 1-2-2v-4l-1-1 1-1V7a2 2 0 0 1 2-2"></path><path d="M18 19a2 2 0 0 0 2-2v-4l1-1-1-1V7a2 2 0 0 0-2-2"></path></svg>',
         class: "pagina",
         onclick: "newWindow('devSettings')",
+        title: "Herramientas avanzadas para desarrolladores",
+        disabled: false
     }
 }
 
@@ -100,11 +126,37 @@ const windowsList = {
     },
     "cookies": {
         title: "Cookies",
-        content: `<p class="intro">Esta página no almacena cookies con datos de carácter personal, privado o identificativo. Todas nuestras cookies son de configuración de lapágina.</p><div class="options"><div class="option"><label for="cookiesEsenciales">Cookies esenciales</label><input type="checkbox" id="cookiesEsenciales" disabled></div><div class="option"><label for="cookiesConfig">Ajustes generales</label><input type="checkbox" id="cookiesConfig"></div><div class="option"><label for="cookiesDev">Ajustes de desarrollador</label><input type="checkbox" id="cookiesDev"></div><div class="option"><button onclick="CookiesRemove(true)">Reiniciar todas las cookies</button></div></div>`
+        content: `<p class="intro">Esta página no almacena cookies con datos de carácter personal, privado o identificativo. Todas nuestras cookies son de configuración de la página.<br>Algunos buscadores no permiten el uso de Cookies.</p><div class="options"><div class="option"><label for="cookiesEsenciales">Cookies esenciales</label><input type="checkbox" id="cookiesEsenciales" disabled></div><div class="option"><label for="cookiesConfig">Ajustes generales</label><input type="checkbox" id="cookiesConfig"></div><div class="option"><label for="cookiesDev">Ajustes de desarrollador</label><input type="checkbox" id="cookiesDev"></div><div class="option"><button onclick="CookiesRemove(true)">Reiniciar todas las cookies</button></div></div>`
     }
 }
 
 function load() {
+
+    if (!CookiesValue("rightClick")) {
+        CookiesAdd("rightClick", "false", "cookiesDev")
+    } else {
+        if (CookiesValue("rightClick") == "false") {
+            document.oncontextmenu = function () { return false }
+        } else if (CookiesValue("rightClick") == "true") {
+            document.oncontextmenu = function () { }
+        } else {
+            CookiesAdd("rightClick", "false", "cookiesDev");
+            document.oncontextmenu = function () { return false }
+        }
+    }
+    if (!CookiesValue("select")) {
+        CookiesAdd("select", "false", "cookiesDev")
+    } else {
+        if (CookiesValue("select") == "false") {
+            document.getElementById("body").classList.remove("select");
+        } else if (CookiesValue("select") == "true") {
+            document.getElementById("body").classList.add("select");
+        } else {
+            CookiesAdd("select", "false", "cookiesDev");
+            document.getElementById("body").classList.remove("select");
+        }
+    }
+
     let headerMenu = [];
     var x = 0;
     for (x = 0; x < (Object.keys(navigation)).length; x++) {
@@ -122,17 +174,37 @@ function load() {
             </div>`
 
         } else if (navigation[`${x}`].class == "pagina") {
-
-            headerMenu[x] = `
-            <div class="pagina" onclick="${navigation[`${x}`].onclick}">
-                <div class="vector">
-                    ${navigation[`${x}`].vector}
-                </div>
-                <div class="nombre">
-                    <p>${navigation[`${x}`].name}</p>
-                </div>
-            </div>`
-
+            if((navigation[`${x}`].name=="Cookies" || navigation[`${x}`].name=="Ajustes" || navigation[`${x}`].name=="Ajustes dev") && warned){
+                headerMenu[x] = `
+                    <div class="pagina disabled" title="El navegador no permite utilizar esta herramienta">
+                        <div class="vector">
+                            ${navigation[`${x}`].vector}
+                        </div>
+                        <div class="nombre">
+                            <p>${navigation[`${x}`].name}</p>
+                        </div>
+                    </div>`
+            } else if (navigation[`${x}`].disabled) {
+                headerMenu[x] = `
+                    <div class="pagina disabled" title="Página deshabilitada por el desarrollador">
+                        <div class="vector">
+                            ${navigation[`${x}`].vector}
+                        </div>
+                        <div class="nombre">
+                            <p>${navigation[`${x}`].name}</p>
+                        </div>
+                    </div>`
+            } else {
+                headerMenu[x] = `
+                    <div class="pagina" onclick="${navigation[`${x}`].onclick}" title="${navigation[`${x}`].title}">
+                        <div class="vector">
+                            ${navigation[`${x}`].vector}
+                        </div>
+                        <div class="nombre">
+                            <p>${navigation[`${x}`].name}</p>
+                        </div>
+                    </div>`
+            }
         }
     }
     document.getElementById("header").innerHTML = `
@@ -163,32 +235,6 @@ function load() {
     mmb.addEventListener("click", function () {
         document.getElementById("header").classList.toggle("active")
     })
-
-    if (!CookiesValue("rightClick")) {
-        CookiesAdd("rightClick", "false", "cookiesDev")
-    } else {
-        if (CookiesValue("rightClick") == "false") {
-            document.oncontextmenu = function () { return false }
-        } else if (CookiesValue("rightClick") == "true") {
-            document.oncontextmenu = function () { }
-        } else {
-            CookiesAdd("rightClick", "false", "cookiesDev");
-            document.oncontextmenu = function () { return false }
-        }
-    }
-    if (!CookiesValue("select")) {
-        CookiesAdd("select", "false", "cookiesDev")
-    } else {
-        if (CookiesValue("select") == "false") {
-            document.getElementById("body").classList.remove("select");
-        } else if (CookiesValue("select") == "true") {
-            document.getElementById("body").classList.add("select");
-        } else {
-            CookiesAdd("select", "false", "cookiesDev");
-            document.getElementById("body").classList.remove("select");
-        }
-    }
-
 }
 
 
@@ -306,8 +352,13 @@ function checkEssentialCookies() {
 }
 
 function CookiesAdd(ruta, valor, type) {
-    checkEssentialCookies()
-    if ((CookiesValue("cookiesDev") == "false" && type == "cookiesDev") || (CookiesValue("cookiesConfig") == "false" && type == "cookiesConfig")) {
+    checkEssentialCookies();
+    if(Object.keys(CookiesComprobar())<1) {
+        if(!warned) {
+            warned=true
+            window.alert("Se ha detectado que su buscador no permite el almacenamiento de Cookies esenciales.\nDe forma automática, todas las funciones de almacenamiento de datos, cookies y otra información se ve paralizada y deshabilitada.")
+        }
+    } else if ((CookiesValue("cookiesDev") == "false" && type == "cookiesDev") || (CookiesValue("cookiesConfig") == "false" && type == "cookiesConfig")) {
         CookiesRemove(ruta)
         return console.info(`No se ha guardado la cookie ${ruta} con valor ${valor} debido a la configuración de cookies`)
     } else {
