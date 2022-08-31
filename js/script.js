9368cbcbb3a67efcd68de9edcdfc74a1e2190253
gacarbla <gabrielcarrob@gmail.com>
2022-08-31 23:37:34 +0200

    Actualización

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ function refreshNavigationBar() {
       class: "pagina",
       onclick: "go(':herramientas/codificador', false)",
       title: "Codificador simple de textos",
-      disabled: true
+      disabled: window.innerWidth<1024
     },
     "GitHub": {
       vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z"></path><path d="M9 19c-4.3 1.4-4.3-2.5-6-3"></path></svg>',
