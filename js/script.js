abe2bbd3d7b4367c09428fa44426d5158dbd1265
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-10-02 23:46:01 +0200

    Actualizar

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@ function refreshNavigationBar() {
       class: "pagina",
       onclick: "go(':herramientas/timestamp', false)",
       title: "Discord Timestamp Generator",
-      disabled: true
+      disabled: false
     },
     "Codificador": {
       vector: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17 11H7a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2Z"></path><path d="M12 17a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path><path d="M8 11V7a4 4 0 0 1 8 0v4"></path></svg>',
