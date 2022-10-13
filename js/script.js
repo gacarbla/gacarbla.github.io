ab53e60563862fa3768efc17ec98066dcb250f8d
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-10-14 00:19:20 +0200

    Actualización

--- js/script.js
+++ js/script.js
@@ -545,7 +545,7 @@ const windows = {
       const windowJSON = await require(`json/windows/${name}.json`, true), structuresJSON = await require(`json/repo/structures.json`, true), camposJSON = await require(`json/repo/campos.json`, true)
       var window = structuresJSON["windows"], campos = []
       for (const x in windowJSON.textos) {
-        if(windowJSON.textos[x].tipo == "radio") { campos.push(windowJSON.campos.replace("%%text%%", windowJSON.textos[x].text[lang]).replace("%%campo%%", camposJSON.windows.radio.replace("%%disabled%%", windowJSON.textos[x].dispo==true?"false":"true").replace("%%hidden%%", windowJSON.textos[x].dispo==true?"hidden":"")).replace(/%%id%%+/g, x)) }
+        if(windowJSON.textos[x].tipo == "radio") { campos.push(windowJSON.campos.replace("%%text%%", windowJSON.textos[x].text[lang]).replace("%%campo%%", camposJSON.windows.radio.replace("%%disabled%%", windowJSON.textos[x].dispo==true?"":"disabled").replace(/%%hidden%%+/g, windowJSON.textos[x].dispo==true?"":"hidden")).replace(/%%id%%+/g, x)) }
       }
       window = window.replace("%%intro%%", windowJSON.intro[lang]).replace("%%campos%%", campos.join(""))
       document.getElementById("windows").innerHTML = `<div class="back" id="${name}"><div class="window ${type ? type : "lateral"}"><svg class="close" onclick="windows.close('${name}')" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg><div class="title"><p>${ventanaData.title[lang]}</p></div><div class="caja">${window}</div></div></div>`
@@ -554,8 +554,8 @@ const windows = {
       if (!windows[`${name}`]) return console.error("No se ha encontrado ninguna ventana con este nombre")
       const divVentanas = document.getElementById("windows")
       divVentanas.innerHTML = `<div class="back" id="${name}"><div class="window ${type ? type : "lateral"}"><svg class="close" onclick="windows.close('${name}')" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg><div class="title"><p>${ventanaData.title[lang]}</p></div><div class="caja">${ventanaData.content[lang]}</div></div></div>`
-      ventanaData.start();
     }
+    ventanaData.start();
   },
   close(name) {
     this.check();
