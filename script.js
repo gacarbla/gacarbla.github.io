5b67e8794d8fdeeeaa5c3d3fd271e1e47e9617e3
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-11-15 01:41:26 +0100

    Actualización

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function start() {
     window.addEventListener("wheel", function(e){
-        console.log(e.target)
         if (document.querySelector('.menu').contains(e.target) || document.querySelector(".pagina").contains(e.target)) {
             if(e.deltaY>=0){
                 document.querySelector(".menu").scrollBy(300, 0)
